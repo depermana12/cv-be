306cab2281695866eb1ff407a820dc5863f491f1
depermana12 <deddiapermana97@gmail.com>
2025-04-06 11:50:08 +0700

    add endpoint for education

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono";
 import { z } from "zod";
 import { zValidator } from "@hono/zod-validator";
-import { Personal } from "../services/cvs";
+import { Personal, Education } from "../services/cvs";
 import { HTTPException } from "hono/http-exception";
 
 const newPersonSchema = z.object({
@@ -14,7 +14,19 @@ const newPersonSchema = z.object({
   bio: z.string().optional(),
 });
 
+const educationSchema = z.object({
+  personalInfoId: z.number().int().optional(),
+  institution: z.string().max(100),
+  degree: z.string().max(100),
+  fieldOfStudy: z.string().max(100).optional(),
+  startDate: z.coerce.date().optional(),
+  endDate: z.coerce.date().optional(),
+  gpa: z.union([z.string(), z.null()]).optional(),
+  maxGpa: z.union([z.string(), z.null()]).optional(),
+});
+
 const personalService = new Personal();
+const educationService = new Education();
 
 export const personalRoute = new Hono();
 personalRoute
@@ -88,7 +100,81 @@ personalRoute
   });
 
 export const educationRoute = new Hono();
-educationRoute.get().post().patch().delete();
+educationRoute
+  .get("/", async (c) => {
+    const education = await educationService.getAll();
+    if (!education) {
+      return c.json({ message: "person not found" }, 404);
+    }
+    return c.json(
+      {
+        message: "success get all education",
+        data: education,
+      },
+      200,
+    );
+  })
+  .post(
+    "/",
+    zValidator("json", educationSchema, (result, c) => {
+      if (!result.success) {
+        throw new HTTPException(400, {
+          message: result.error.issues[0].message,
+        });
+      }
+    }),
+    async (c) => {
+      const validated = c.req.valid("json");
+      const newEducation = await educationService.create(validated);
+      return c.json(
+        {
+          message: "new education created",
+          data: newEducation,
+        },
+        201,
+      );
+    },
+  )
+  .patch(
+    "/:id",
+    zValidator("json", educationSchema, (result, c) => {
+      if (!result.success) {
+        throw new HTTPException(400, {
+          message: result.error.issues[0].message,
+        });
+      }
+    }),
+    async (c) => {
+      const eduId = c.req.param("id");
+      const validated = c.req.valid("json");
+
+      const getEdu = await educationService.getById(Number(eduId));
+      if (!getEdu) {
+        return c.json({ message: "education id not found" }, 404);
+      }
+      await educationService.update(Number(eduId), validated);
+      const updatedEdu = await educationService.getById(Number(eduId));
+      return c.json(
+        {
+          message: "education has been updated",
+          data: updatedEdu,
+        },
+        200,
+      );
+    },
+  )
+  .delete("/:id", async (c) => {
+    const eduId = c.req.param("id");
+    const eduToBeDeleted = await educationService.getById(Number(eduId));
+    if (!eduToBeDeleted) {
+      return c.json({ message: "edu not found" }, 404);
+    }
+    await educationService.delete(Number(eduId));
+    return c.json({
+      message: "this education has been deleted",
+      data: eduToBeDeleted,
+    });
+  });
 
 export const workExpRoute = new Hono();
 workExpRoute.get().post().patch().delete();
--- src/services/cvs.ts
+++ src/services/cvs.ts
@@ -15,9 +15,12 @@ import {
   softSkills,
   courses,
   courseDetails,
+  educationRelations,
 } from "../db/schema";
+import { HTTPException } from "hono/http-exception";
 
 type PersonalType = typeof personalInfo.$inferInsert;
+type EducationType = typeof education.$inferInsert;
 export class Personal {
   async get() {
     try {
@@ -75,49 +78,97 @@ export class Personal {
 }
 
 export class Education {
-  async getAll() {}
-  async getById(id) {}
-  async create(eduData) {}
-  async update(id, newEduData) {}
-  async delete(id) {}
+  async getAll() {
+    try {
+      const rows = await db.select().from(education);
+      return rows[0];
+    } catch (e: unknown) {
+      throw new Error(e instanceof Error ? e.message : String(e));
+    }
+  }
+  async getById(id: number) {
+    try {
+      const educationRows = await db
+        .select()
+        .from(education)
+        .where(eq(education.id, Number(id)));
+      if (!education) {
+        throw new HTTPException(404, {
+          message: "invalid education id not found",
+        });
+      }
+      return educationRows[0];
+    } catch (e: unknown) {
+      throw new Error(e instanceof Error ? e.message : String(e));
+    }
+  }
+  async create(eduData: EducationType) {
+    try {
+      const newEducation = await db
+        .insert(education)
+        .values(eduData)
+        .$returningId();
+      return newEducation[0];
+    } catch (e: unknown) {
+      throw new Error(e instanceof Error ? e.message : String(e));
+    }
+  }
+  async update(id: number, newEduData: EducationType) {
+    try {
+      const rows = await db
+        .update(education)
+        .set(newEduData)
+        .where(eq(education.id, Number(id)));
+      return rows[0];
+    } catch (e: unknown) {
+      throw new Error(e instanceof Error ? e.message : String(e));
+    }
+  }
+  async delete(id: number) {
+    try {
+      await db.delete(education).where(eq(education.id, Number(id)));
+    } catch (e: unknown) {
+      throw new Error(e instanceof Error ? e.message : String(e));
+    }
+  }
 }
 
 export class WorkExp {
   async getAll() {}
-  async getById(id) {}
+  async getById(id: number) {}
   async create(experience) {}
-  async update(id, newExperience) {}
+  async update(id: number, newExperience) {}
   async addDetails(workExpId, newWorkExp) {}
   async updateDetails(detailId, newDetailExp) {}
-  async delete(id) {}
+  async delete(id: number) {}
 }
 
 export class OrgExp {
   async getAll() {}
-  async getById(id) {}
+  async getById(id: number) {}
   async create(orgExp) {}
-  async update(id, newOrgExp) {}
+  async update(id: number, newOrgExp) {}
   async addDetails(orgExpId, newOrgExp) {}
   async updateDetails(detailId, newDetailExp) {}
-  async delete(id) {}
+  async delete(id: number) {}
 }
 
 export class Project {
   async getAll() {}
-  async getById(id) {}
+  async getById(id: number) {}
   async create(project) {}
-  async update(id, newProjectData) {}
+  async update(id: number, newProjectData) {}
   async addDetails(projectId, newProjectDetail) {}
   async updateDetails(detailId, newDetail) {}
-  async delete(id) {}
+  async delete(id: number) {}
 }
 
 export class Course {
   async getAll() {}
-  async getById(id) {}
+  async getById(id: number) {}
   async create(course) {}
-  async update(id, newCourseData) {}
+  async update(id: number, newCourseData) {}
   async addDetails(courseId, newCourseDetail) {}
   async updateDetails(detailId, newDetail) {}
-  async delete(id) {}
+  async delete(id: number) {}
 }
