500f685c22618a4eab13b4cc6248d6da07be717a
depermana12 <deddiapermana97@gmail.com>
2025-05-03 12:36:53 +0700

    fix: regex on phone, and add message

--- src/schemas/personal.schema.ts
+++ src/schemas/personal.schema.ts
@@ -4,14 +4,17 @@ const idSchema = z.number().int().positive();
 
 export const personalSelectSchema = z.object({
   id: idSchema,
+  userId: idSchema,
   fullName: z
     .string()
     .min(3, { message: "Must be 3 or more characters long" })
     .max(100),
   bio: z.string().max(255, { message: "Must be 255 characters or fewer" }),
   image: z.string().max(255, { message: "Must be 255 characters or fewer" }),
-  summary: z.string(),
-  phone: z.string().regex(/^\+?[1-9]\d{1,14}$/),
+  summary: z.string({ message: "summary needed" }),
+  phone: z.string().regex(/^(\+\d{1,3}|0)[1-9]\d{4,14}$/, {
+    message: "Must start with '+' followed by country code or '0'",
+  }),
   email: z.string().email({ message: "Invalid email address" }),
   url: z.string().url({ message: "Invalid url" }),
   createdAt: z.string().datetime(),
@@ -19,6 +22,7 @@ export const personalSelectSchema = z.object({
 
 export const personalInsertSchema = personalSelectSchema.omit({
   id: true,
+  userId: true,
   createdAt: true,
 });
 
