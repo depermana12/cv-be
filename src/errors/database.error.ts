a1a88db9c15d6b406936671d391ae94b04a0a798
depermana12 <deddiapermana97@gmail.com>
2025-05-03 18:39:45 +0700

    fix prototype chain

--- src/errors/database.error.ts
+++ src/errors/database.error.ts
@@ -2,5 +2,6 @@ export class DataBaseError extends Error {
   constructor(message: string) {
     super(message);
     this.name = "DataBaseError";
+    Object.setPrototypeOf(this, DataBaseError.prototype);
   }
 }
