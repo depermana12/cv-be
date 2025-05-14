636ad6b77ebe92aca1d27e6fb1397c78fad284ae
depermana12 <deddiapermana97@gmail.com>
2025-05-14 19:05:45 +0700

    refactor: using drizzle relational queries

--- src/db/index.ts
+++ src/db/index.ts
@@ -8,4 +8,36 @@ export async function initializeDb() {
     connectionLimit: 10,
   });
 }
-export const db = drizzle(process.env.DATABASE_URL!);
+
+import * as user from "./schema/user.db";
+import * as personal from "./schema/personal.db";
+import * as language from "./schema/language.db";
+import * as education from "./schema/education.db";
+import * as location from "./schema/location.db";
+import * as social from "./schema/social.db";
+import * as work from "./schema/work.db";
+import * as organization from "./schema/organization.db";
+import * as project from "./schema/project.db";
+import * as skill from "./schema/skill.db";
+import * as softSkill from "./schema/soft-skill.db";
+import * as course from "./schema/course.db";
+
+const schema = {
+  ...user,
+  ...personal,
+  ...language,
+  ...education,
+  ...location,
+  ...social,
+  ...work,
+  ...organization,
+  ...project,
+  ...skill,
+  ...softSkill,
+  ...course,
+};
+
+export const db = drizzle(process.env.DATABASE_URL!, {
+  schema,
+  mode: "default",
+});
