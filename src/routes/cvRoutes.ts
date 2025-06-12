636eaea3f4ade256b750d51a20f90c328399ac68
depermana12 <deddiapermana97@gmail.com>
2025-06-12 16:50:02 +0700

    refactor(routes): organize route inside controller

--- src/routes/cvRoutes.ts
+++ src/routes/cvRoutes.ts
@@ -5,25 +5,25 @@ import { educationRoutes } from "../controllers/education.controller";
 import { languageRoutes } from "../controllers/language.controller";
 import { locationRoutes } from "../controllers/location.controller";
 import { organizationRoutes } from "../controllers/organization.controller";
-import { personalRoutes } from "../controllers/personal.controller";
 import { projectRoutes } from "../controllers/project.controller";
 import { skillRoutes } from "../controllers/skill.controller";
-import { socialRoutes } from "../controllers/social.controller";
 import { softSkillRoutes } from "../controllers/soft-skill.controller";
 import { workRoutes } from "../controllers/work.controller";
+import { socialRoutes } from "../controllers/socialMedia.controller";
+import { profileRoutes } from "../controllers/profile.controller";
 
 const cvChildRoutes = new Hono();
 
-cvChildRoutes.route("/:cvId/personals", personalRoutes);
-cvChildRoutes.route("/:cvId/locations", locationRoutes);
-cvChildRoutes.route("/:cvId/socials", socialRoutes);
-cvChildRoutes.route("/:cvId/languages", languageRoutes);
-cvChildRoutes.route("/:cvId/education", educationRoutes);
-cvChildRoutes.route("/:cvId/works", workRoutes);
-cvChildRoutes.route("/:cvId/organizations", organizationRoutes);
-cvChildRoutes.route("/:cvId/projects", projectRoutes);
-cvChildRoutes.route("/:cvId/courses", courseRoutes);
-cvChildRoutes.route("/:cvId/skills", skillRoutes);
-cvChildRoutes.route("/:cvId/soft-skills", softSkillRoutes);
+cvChildRoutes.route("/", profileRoutes);
+cvChildRoutes.route("/", locationRoutes);
+cvChildRoutes.route("/", socialRoutes);
+cvChildRoutes.route("/", languageRoutes);
+cvChildRoutes.route("/", educationRoutes);
+cvChildRoutes.route("/", workRoutes);
+cvChildRoutes.route("/", organizationRoutes);
+cvChildRoutes.route("/", projectRoutes);
+cvChildRoutes.route("/", courseRoutes);
+cvChildRoutes.route("/", skillRoutes);
+cvChildRoutes.route("/", softSkillRoutes);
 
 export default cvChildRoutes;
