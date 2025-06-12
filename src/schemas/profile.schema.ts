74bf09ccce1a7f8c71df27c7add6569c600379b1
depermana12 <deddiapermana97@gmail.com>
2025-06-12 11:11:48 +0700

    refactor(controller): reflect personal to service

--- src/schemas/profile.schema.ts
+++ src/schemas/profile.schema.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 const idSchema = z.number().int().positive();
 
-export const personalSelectSchema = z.object({
+export const profileSelectSchema = z.object({
   id: idSchema,
   cvId: idSchema,
   firstName: z
@@ -23,13 +23,20 @@ export const personalSelectSchema = z.object({
   url: z.string().url({ message: "Invalid url" }),
 });
 
-export const personalInsertSchema = personalSelectSchema.omit({
+export const profileInsertSchema = profileSelectSchema.omit({
   id: true,
   cvId: true,
 });
 
-export const personalUpdateSchema = personalInsertSchema.partial();
+export const profileUpdateSchema = profileInsertSchema.partial();
 
-export type PersonalSelect = z.infer<typeof personalSelectSchema>;
-export type PersonalInsert = z.infer<typeof personalInsertSchema>;
-export type PersonalUpdate = z.infer<typeof personalUpdateSchema>;
+export const profileQueryOptionsSchema = z.object({
+  search: z.string().optional(),
+  sortBy: z.enum(["firstName", "lastName"]).optional(),
+  sortOrder: z.enum(["asc", "desc"]).optional(),
+});
+
+export type ProfileSelect = z.infer<typeof profileSelectSchema>;
+export type ProfileInsert = z.infer<typeof profileInsertSchema>;
+export type ProfileUpdate = z.infer<typeof profileUpdateSchema>;
+export type ProfileQueryOptions = z.infer<typeof profileQueryOptionsSchema>;
