32881d6db9744df6a366db88c6cc12dd51240411
depermana12 <deddiapermana97@gmail.com>
2025-07-13 16:17:49 +0700

    feat(tracker status): add service layer for add an update status and get statuses

--- src/services/jobApplication.service.ts
+++ src/services/jobApplication.service.ts
@@ -1,23 +1,18 @@
-import type {
-  IJobApplication,
-  JobApplicationRepository,
-} from "../repositories/jobApplication.repo";
+import type { IJobApplication } from "../repositories/jobApplication.repo";
 import type {
   JobApplicationInsert,
   JobApplicationQueryOptions,
   JobApplicationSelect,
+  JobApplicationStatusSelect,
   JobApplicationUpdate,
   PaginatedJobApplicationResponse,
 } from "../db/types/jobApplication.type";
 import { NotFoundError } from "../errors/not-found.error";
+import type { IJobApplicationStatus } from "../repositories/jobApplicationStatus.repo";
 
 export interface IJobApplicationService {
   createJobApplication(
     data: Omit<JobApplicationInsert, "userId">,
-    userId: number,
-  ): Promise<JobApplicationSelect>;
-  getJobApplicationById(
-    id: number,
     userId: number,
   ): Promise<JobApplicationSelect>;
   getJobApplicationById(
@@ -33,21 +28,24 @@ export interface IJobApplicationService {
     userId: number,
     newData: JobApplicationUpdate,
   ): Promise<JobApplicationSelect>;
+  getStatusTimeline(
+    applicationId: number,
+    userId: number,
+  ): Promise<JobApplicationStatusSelect[]>;
   deleteJobApplication(id: number, userId: number): Promise<boolean>;
 }
 
 export class JobApplicationService {
-  constructor(private readonly jobApplicationRepository: IJobApplication) {}
+  constructor(
+    private readonly repo: IJobApplication,
+    private readonly statusRepo: IJobApplicationStatus,
+  ) {}
 
-  private async assertJobApplicationOwnedByUser(
+  private async assertOwned(
     id: number,
     userId: number,
   ): Promise<JobApplicationSelect> {
-    const app =
-      await this.jobApplicationRepository.getJobApplicationByIdAndUserId(
-        id,
-        userId,
-      );
+    const app = await this.repo.getJobApplicationByIdAndUserId(id, userId);
     if (!app) {
       throw new NotFoundError(
         `[Service] Job Application with ID ${id} not found for user ${userId}`,
@@ -60,10 +58,17 @@ export class JobApplicationService {
     data: Omit<JobApplicationInsert, "userId">,
     userId: number,
   ): Promise<JobApplicationSelect> {
-    const { id } = await this.jobApplicationRepository.createJobApplication({
+    const { id } = await this.repo.createJobApplication({
       ...data,
       userId,
     });
+
+    await this.statusRepo.addStatus(id, {
+      applicationId: id,
+      status: data.status ?? "applied",
+      changedAt: data.appliedAt,
+    });
+
     return this.getJobApplicationById(id, userId);
   }
 
@@ -71,31 +76,40 @@ export class JobApplicationService {
     id: number,
     userId: number,
   ): Promise<JobApplicationSelect> {
-    return this.assertJobApplicationOwnedByUser(id, userId);
+    return this.assertOwned(id, userId);
   }
 
   async getAllJobApplications(
     userId: number,
     options?: JobApplicationQueryOptions,
   ): Promise<PaginatedJobApplicationResponse> {
-    return this.jobApplicationRepository.getAllJobApplicationsByUserId(
-      userId,
-      options,
-    );
+    return this.repo.getAllJobApplicationsByUserId(userId, options);
   }
 
   async updateJobApplication(
     id: number,
     userId: number,
     newData: JobApplicationUpdate,
+    statusChangeAt?: Date,
   ): Promise<JobApplicationSelect> {
-    const app = await this.assertJobApplicationOwnedByUser(id, userId);
-    const updated =
-      await this.jobApplicationRepository.updateJobApplicationByIdAndUserId(
-        app.id,
-        userId,
-        newData,
-      );
+    const job = await this.assertOwned(id, userId);
+
+    const hasStatusChanged = newData.status && newData.status !== job.status;
+
+    if (hasStatusChanged) {
+      const changedAt = statusChangeAt ?? new Date();
+      await this.statusRepo.addStatus(id, {
+        applicationId: id,
+        status: newData.status!,
+        changedAt,
+      });
+    }
+
+    const updated = await this.repo.updateJobApplicationByIdAndUserId(
+      id,
+      userId,
+      newData,
+    );
     if (!updated) {
       throw new NotFoundError(
         `[Service] Job Application with ID ${id} not found for user ${userId}`,
@@ -104,10 +118,12 @@ export class JobApplicationService {
     return this.getJobApplicationById(id, userId);
   }
 
+  async getStatusTimeline(applicationId: number, userId: number) {
+    await this.assertOwned(applicationId, userId);
+    return this.statusRepo.getStatuses(applicationId);
+  }
+
   async deleteJobApplication(id: number, userId: number): Promise<boolean> {
-    return this.jobApplicationRepository.deleteJobApplicationByIdAndUserId(
-      id,
-      userId,
-    );
+    return this.repo.deleteJobApplicationByIdAndUserId(id, userId);
   }
 }
