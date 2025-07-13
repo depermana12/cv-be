17be90751c7461fab242b6ceed24d4f8a2eca48e
depermana12 <deddiapermana97@gmail.com>
2025-07-13 10:32:50 +0700

    feat(job statuses): add types job statuses

--- src/db/types/jobApplication.type.ts
+++ src/db/types/jobApplication.type.ts
@@ -1,9 +1,20 @@
-import { jobApplications } from "../schema/jobApplication.db";
+import {
+  jobApplications,
+  jobApplicationStatuses,
+} from "../schema/jobApplication.db";
 
 export type JobApplicationSelect = typeof jobApplications.$inferSelect;
 export type JobApplicationInsert = typeof jobApplications.$inferInsert;
 export type JobApplicationUpdate = Partial<
   Omit<JobApplicationInsert, "id" | "cvId" | "userId">
+>;
+
+export type JobApplicationStatusSelect =
+  typeof jobApplicationStatuses.$inferSelect;
+export type JobApplicationStatusInsert =
+  typeof jobApplicationStatuses.$inferInsert;
+export type JobApplicationStatusUpdate = Partial<
+  Omit<JobApplicationInsert, "id" | "applicationId">
 >;
 
 export type JobApplicationQueryOptions = {
