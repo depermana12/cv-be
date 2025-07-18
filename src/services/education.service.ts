e5d3a266effe5a8ee296bf2ba4097f9b4d8aae0d
depermana12 <deddiapermana97@gmail.com>
2025-07-18 23:10:53 +0700

    refactor: simplify education service

--- src/services/education.service.ts
+++ src/services/education.service.ts
@@ -4,13 +4,11 @@ import type {
   EducationInsert,
   EducationQueryOptions,
   EducationSelect,
-  EducationUpdate,
 } from "../db/types/education.type";
 
 export class EducationService extends CvChildService<
   EducationSelect,
-  EducationInsert,
-  EducationUpdate
+  EducationInsert
 > {
   constructor(private readonly educationRepository: EducationRepository) {
     super(educationRepository);
@@ -20,26 +18,16 @@ export class EducationService extends CvChildService<
     cvId: number,
     educationData: Omit<EducationInsert, "cvId">,
   ): Promise<EducationSelect> {
-    return this.createForCv(cvId, { ...educationData, cvId });
+    return this.createInCv(cvId, { ...educationData, cvId });
   }
 
   async getEducation(
     cvId: number,
     educationId: number,
   ): Promise<EducationSelect> {
-    return this.findByCvId(cvId, educationId);
+    return this.getByIdInCv(cvId, educationId);
   }
 
-  /**
-   * Get all education entries for a specific CV.
-   * Uses method from the repository not cvChild service.
-   * @param cvId - The ID of the CV.
-   * @param options - Query options for filtering and sorting.
-   * @param options.search - Optional search term to filter by institution name.
-   * @param options.sortBy - Optional field to sort by (e.g., "startDate", "endDate").
-   * @param options.sortOrder - Optional sort order ("asc" or "desc").
-   * @returns A promise that resolves to an array of EducationSelect objects.
-   */
   async getAllEducations(
     cvId: number,
     options?: EducationQueryOptions,
@@ -50,12 +38,12 @@ export class EducationService extends CvChildService<
   async updateEducation(
     cvId: number,
     educationId: number,
-    newEducationData: Omit<EducationUpdate, "cvId">,
+    newEducationData: Omit<EducationInsert, "cvId">,
   ): Promise<EducationSelect> {
-    return this.updateForCv(cvId, educationId, newEducationData);
+    return this.updateInCv(cvId, educationId, newEducationData);
   }
 
   async deleteEducation(cvId: number, educationId: number): Promise<boolean> {
-    return this.deleteFromCv(cvId, educationId);
+    return this.deleteInCv(cvId, educationId);
   }
 }
