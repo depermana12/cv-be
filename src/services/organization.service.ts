fe38fcc0746522d06a12888aa2aa06c41ced1da5
depermana12 <deddiapermana97@gmail.com>
2025-07-19 12:48:49 +0700

    refactor: streamline OrganizationService by removing unused methods and types

--- src/services/organization.service.ts
+++ src/services/organization.service.ts
@@ -1,173 +1,73 @@
 import { CvChildService } from "./cvChild.service";
 import type {
-  OrganizationDescInsert,
-  OrganizationDescSelect,
-  OrganizationDescUpdate,
   OrganizationInsert,
   OrganizationQueryOptions,
   OrganizationSelect,
-  OrganizationUpdate,
-  OrganizationResponse,
-  OrganizationCreateRequest,
-  OrganizationUpdateRequest,
 } from "../db/types/organization.type";
-import { NotFoundError } from "../errors/not-found.error";
-import { BadRequestError } from "../errors/bad-request.error";
 import { OrganizationRepository } from "../repositories/organization.repo";
 
-export class OrganizationService extends CvChildService<
-  OrganizationSelect,
-  OrganizationInsert,
-  OrganizationUpdate
-> {
+export interface IOrganizationService {
+  createOrganization(
+    cvId: number,
+    organizationData: Omit<OrganizationInsert, "cvId">,
+  ): Promise<OrganizationSelect>;
+  getOrganization(
+    cvId: number,
+    organizationId: number,
+  ): Promise<OrganizationSelect>;
+  getAllOrganizations(
+    cvId: number,
+    options?: OrganizationQueryOptions,
+  ): Promise<OrganizationSelect[]>;
+  updateOrganization(
+    cvId: number,
+    organizationId: number,
+    updateData: Omit<OrganizationInsert, "cvId">,
+  ): Promise<OrganizationSelect>;
+  deleteOrganization(cvId: number, organizationId: number): Promise<boolean>;
+}
+
+export class OrganizationService
+  extends CvChildService<OrganizationSelect, OrganizationInsert>
+  implements IOrganizationService
+{
   constructor(private readonly organizationRepository: OrganizationRepository) {
     super(organizationRepository);
   }
 
-  async getAllOrganizations(
+  async createOrganization(
     cvId: number,
-    options?: OrganizationQueryOptions,
-  ): Promise<OrganizationResponse[]> {
-    return this.organizationRepository.getAllOrganizations(cvId, options);
+    organizationData: Omit<OrganizationInsert, "cvId">,
+  ): Promise<OrganizationSelect> {
+    return this.createInCv(cvId, { ...organizationData, cvId });
   }
 
   async getOrganization(
     cvId: number,
     organizationId: number,
-  ): Promise<OrganizationResponse> {
-    const organization = await this.organizationRepository.getOrganization(
-      organizationId,
-    );
-    if (!organization || organization.cvId !== cvId) {
-      throw new NotFoundError(
-        `Organization ${organizationId} not found for CV ${cvId}`,
-      );
-    }
-    return organization;
+  ): Promise<OrganizationSelect> {
+    return this.getByIdInCv(cvId, organizationId);
   }
 
-  async createOrganization(
+  async getAllOrganizations(
     cvId: number,
-    organizationData: OrganizationCreateRequest,
-  ): Promise<OrganizationResponse> {
-    const { descriptions = [], ...restOrganizationData } = organizationData;
-
-    const result = await this.organizationRepository.createOrganization(
-      { ...restOrganizationData, cvId },
-      descriptions,
-    );
-
-    return this.getOrganization(cvId, result.id);
+    options?: OrganizationQueryOptions,
+  ): Promise<OrganizationSelect[]> {
+    return this.organizationRepository.getAllOrganizations(cvId, options);
   }
 
   async updateOrganization(
     cvId: number,
     organizationId: number,
-    updateData: OrganizationUpdateRequest,
-  ): Promise<OrganizationResponse> {
-    await this.getOrganization(cvId, organizationId);
-
-    const { descriptions, ...organizationData } = updateData;
-    await this.organizationRepository.updateOrganization(
-      organizationId,
-      organizationData,
-      descriptions,
-    );
-
-    return this.getOrganization(cvId, organizationId);
+    updateData: Omit<OrganizationInsert, "cvId">,
+  ): Promise<OrganizationSelect> {
+    return this.updateInCv(cvId, organizationId, updateData);
   }
 
   async deleteOrganization(
     cvId: number,
     organizationId: number,
   ): Promise<boolean> {
-    await this.getOrganization(cvId, organizationId);
-    return this.organizationRepository.deleteOrganization(organizationId);
-  }
-
-  async getAllDescriptions(
-    cvId: number,
-    organizationId: number,
-  ): Promise<OrganizationDescSelect[]> {
-    await this.getOrganization(cvId, organizationId);
-    return this.organizationRepository.getAllDescriptions(organizationId);
-  }
-
-  async addDescription(
-    cvId: number,
-    organizationId: number,
-    descriptionData: Omit<OrganizationDescInsert, "organizationId">,
-  ): Promise<OrganizationDescSelect> {
-    await this.getOrganization(cvId, organizationId);
-    const result = await this.organizationRepository.addDescription(
-      organizationId,
-      descriptionData,
-    );
-
-    if (!result) {
-      throw new BadRequestError(
-        `Failed to add description to organization ${organizationId}`,
-      );
-    }
-
-    return this.getDescription(cvId, result.id);
-  }
-
-  async getDescription(
-    cvId: number,
-    descriptionId: number,
-  ): Promise<OrganizationDescSelect> {
-    const description = await this.organizationRepository.getDescription(
-      descriptionId,
-    );
-    if (!description) {
-      throw new NotFoundError(`Description ${descriptionId} not found`);
-    }
-
-    // Verify organization belongs to CV
-    await this.getOrganization(cvId, description.organizationId);
-    return description;
-  }
-
-  async updateDescription(
-    cvId: number,
-    descriptionId: number,
-    updateData: OrganizationDescUpdate,
-  ): Promise<OrganizationDescSelect> {
-    const description = await this.organizationRepository.getDescription(
-      descriptionId,
-    );
-    if (!description) {
-      throw new NotFoundError(`Description ${descriptionId} not found`);
-    }
-
-    await this.getOrganization(cvId, description.organizationId);
-
-    const updated = await this.organizationRepository.updateDescription(
-      descriptionId,
-      updateData,
-    );
-    if (!updated) {
-      throw new BadRequestError(
-        `Failed to update description ${descriptionId}`,
-      );
-    }
-
-    return this.getDescription(cvId, descriptionId);
-  }
-
-  async deleteDescription(
-    cvId: number,
-    descriptionId: number,
-  ): Promise<boolean> {
-    const description = await this.organizationRepository.getDescription(
-      descriptionId,
-    );
-    if (!description) {
-      throw new NotFoundError(`Description ${descriptionId} not found`);
-    }
-
-    await this.getOrganization(cvId, description.organizationId);
-    return this.organizationRepository.deleteDescription(descriptionId);
+    return this.deleteInCv(cvId, organizationId);
   }
 }
